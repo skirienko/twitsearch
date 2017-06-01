6e3a60bdf7855a5641cab263350a71ea81697acb
Sergey Kirienko <skirienko@iponweb.net>
2017-06-01 13:57:19 +0300

    Added 'Load more' button, removed console debug

--- public/app.module.js
+++ public/app.module.js
@@ -8,21 +8,33 @@ angular.module('twitsearch').controller('mainController', ['$scope', '$http', fu
 
     $scope.query = "";
     $scope.tweets = [];
+    $scope.lastQuery = '';
 
     $scope.search = function () {
-        console.log("BANG!");
-        console.log($scope.query);
 
-        $http.get('/search?q=' + window.encodeURIComponent($scope.query))
+        $scope.lastQuery = '/search?q=' + window.encodeURIComponent($scope.query);
+        $http.get($scope.lastQuery)
             .then(
                 function(response) {
-                    console.log(response);
+	                $scope.meta = response.data.search_metadata;
                     $scope.tweets = response.data.statuses;
                 },
                 function(error) {
                     console.log(error);
                 }
             );
+    };
 
+    $scope.addMoreTweets = function() {
+	    $scope.lastQuery = '/search' + $scope.meta.next_results;
+        $http.get($scope.lastQuery)
+            .then(
+                function(response) {
+                    if (response.statusText === 'OK') {
+                        $scope.tweets = $scope.tweets.concat(response.data.statuses);
+	                    $scope.meta = response.data.search_metadata;
+                    }
+                }
+            );
     };
 }]);
